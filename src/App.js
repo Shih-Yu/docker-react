a4440d78b5d702ca1ef7c36e4ba2ae9754cc11ea
Shih-Yu Hwang <shihyuhwang@gmail.com>
2023-05-15 19:14:11 -0500

    changed test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>BYE  </p>
+        <p>BYE NOW! </p>
         <a
           className='App-link'
           href='https://reactjs.org'
